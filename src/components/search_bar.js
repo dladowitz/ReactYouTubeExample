fede181f7092aaa0595cde174d619c8c2ac7668f
David Ladowitz <david@ladowitz.com>
2016-09-26 12:12:24 -0700

    getting state working on search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,10 +4,26 @@ import React, { Component } from 'react'
 
 // class based component
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: ' '};
+  }
+
   // always need a render function
   render(){ //slightly different syntax for creating a function
-    return <input  onChange={ event => console.log(event.target.value) } />;
+    return (
+      <div>
+        <input  onChange={ event => this.setState({ term: event.target.value }) } />
+        Value of the input: { this.state.term }
+      </div>
+    );
   }
 }
+
+//State: JS object used to record and react to events.
+// Each class based compent has it's own state object.
+// Anytime a components state is changed it run the render function along with
+// its child objects.
 
 export default SearchBar;
