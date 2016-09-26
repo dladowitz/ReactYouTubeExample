abcd04f224976c309f013aa886cac86ac3bbb245
David Ladowitz <david@ladowitz.com>
2016-09-26 11:07:28 -0700

    added search bar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+// imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+// test API key - DISABLE or move to ENV Var
+const API_Key = 'AIzaSyCae9rBWFqOXviUYTU1qB0XHmL1y5NMiMM'
+
 
 // Create a new compent to produce hTML
 const App = () => {
-  return <div> Hi! </div>
+  return <div>
+    <SearchBar />
+  </div>
 }
 
 // Take this compent's generated HTML and put into the DOM
