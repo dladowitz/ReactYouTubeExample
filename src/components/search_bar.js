900415a358a660ff03d84b2da52aa9e946ca5802
David Ladowitz <david@ladowitz.com>
2016-09-26 11:52:57 -0700

    refactoring search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
+// set var called Component from React.Component
+// const Component = React.Component
 
-const SearchBar = () => {
-  return <input />
+// class based component
+class SearchBar extends Component {
+  // always need a render function
+  render(){ //slightly different syntax for creating a function
+    return <input  onChange={ event => console.log(event.target.value) } />;
+  }
 }
 
 export default SearchBar;
