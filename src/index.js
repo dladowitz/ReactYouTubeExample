5e9801cac9ede63cb07179c321348394b8f7157f
David Ladowitz <david@ladowitz.com>
2016-09-26 15:51:59 -0700

    getting youtube api working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-// imports
-import React from 'react';
+// library imports
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+// component imports
 import SearchBar from './components/search_bar';
 
 // test API key - DISABLE or move to ENV Var
@@ -8,10 +11,25 @@ const API_Key = 'AIzaSyCae9rBWFqOXviUYTU1qB0XHmL1y5NMiMM'
 
 
 // Create a new compent to produce hTML
-const App = () => {
-  return <div>
-    <SearchBar />
-  </div>
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [1, 2, 3] };
+    // console.log(this.state)
+
+    YTSearch({key: API_Key, term: 'surfboards'}, (data) => {
+      this.setState({ videos: data });
+      console.log(this.state);
+    });
+  }
+
+  render(){
+    return <div>
+      <SearchBar />
+    </div>
+
+  }
 }
 
 // Take this compent's generated HTML and put into the DOM
