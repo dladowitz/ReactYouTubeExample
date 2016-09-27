4bef0dd35d077361973042fc63d4f2718b7d36fe
David Ladowitz <david@ladowitz.com>
2016-09-26 22:54:06 -0700

    adding video detailsgs

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,7 +7,7 @@ class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = { term: 'hello world'};
+    this.state = { term: 'Search for Videos'};
   }
 
   // always need a render function
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import YTSearch from 'youtube-api-search';
 
 // component imports
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // test API key - DISABLE or move to ENV Var
 const API_Key = 'AIzaSyCae9rBWFqOXviUYTU1qB0XHmL1y5NMiMM'
@@ -15,20 +17,33 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: [1, 2, 3] };
-    // console.log(this.state)
+    this.state = { videos: [
+        {id: {videoId: 1}, etag: 1, snippet: {title: "Die Hard", description: "Bruce Willis Kills Terrorists", thumbnails: { default: {url: "./images/zebra.jpg"} }}},
+        {id: {videoId: 1}, etag: 2, snippet: {title: "Die Hard 2", description: "Bruce Willis Kills Terrorists", thumbnails: { default: {url: "./images/zebra.jpg"} }}},
+        {id: {videoId: 1}, etag: 3, snippet: {title: "Die Hard 3", description: "Bruce Willis Kills Terrorists", thumbnails: { default: {url: "./images/zebra.jpg"} }}}
+      ] };
+    console.log(this.state)
 
-    YTSearch({key: API_Key, term: 'surfboards'}, (data) => {
-      this.setState({ videos: data });
-      console.log(this.state);
+    YTSearch({key: API_Key, term: 'surfboards'}, (videos) => {d
+      if(vidoes){
+        console.log("Found Vidoes")
+        this.setState({ videos });
+
+      }else{
+        console.log("Can't find videos")
+        this.setState({ videos: ["Die Hard", "Die Hard 2", "Die Hard 3"]})
+      }
     });
   }
 
-  render(){
-    return <div>
-      <SearchBar />
-    </div>
-
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
   }
 }
 
